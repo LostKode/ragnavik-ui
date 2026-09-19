a075889fbf078c255fb0b91ba89cd6bc7ae0271f
Daniel Cornelison <itsklastic@gmail.com>
2026-09-19 02:23:58 -0500

    chore: release UI 1.2.5

--- src/RagnavikUI.cs
+++ src/RagnavikUI.cs
@@ -15,7 +15,7 @@ using UnityEngine.UI;
 
 namespace RagnavikUI;
 
-[BepInPlugin("lostkode.ragnavik.ui", "Ragnavik UI", "1.2.4")]
+[BepInPlugin("lostkode.ragnavik.ui", "Ragnavik UI", "1.2.5")]
 public sealed class RagnavikUIPlugin : BaseUnityPlugin
 {
     internal const string PluginGuid = "lostkode.ragnavik.ui";
