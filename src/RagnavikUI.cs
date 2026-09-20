ff987dfa4e7e7c275a4c0f1f7da8ff9c6ffce220
Daniel Cornelison <itsklastic@gmail.com>
2026-09-19 21:55:15 -0500

    chore: release UI 1.2.8
    Publish the player facing store details with aligned package and plugin version metadata and a standalone changelog entry.

--- src/RagnavikUI.cs
+++ src/RagnavikUI.cs
@@ -15,7 +15,7 @@ using UnityEngine.UI;
 
 namespace RagnavikUI;
 
-[BepInPlugin("lostkode.ragnavik.ui", "Ragnavik UI", "1.2.7")]
+[BepInPlugin("lostkode.ragnavik.ui", "Ragnavik UI", "1.2.8")]
 public sealed class RagnavikUIPlugin : BaseUnityPlugin
 {
     internal const string PluginGuid = "lostkode.ragnavik.ui";
